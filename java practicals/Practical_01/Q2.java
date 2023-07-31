a7bf28d81afe8286e505c211e68e2d94eeb54ef4
Hasiniag <141015631+Hasiniag@users.noreply.github.com>
2023-07-31 21:53:00 +0530

    Add files via upload

---
+++ java practicals/Practical_01/Q2.java
@@ -0,0 +1,12 @@
+
+
+package com.mycompany.q2;
+
+
+public class Q2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hasini AG");
+        System.out.println("Computer Networks");
+    }
+}
